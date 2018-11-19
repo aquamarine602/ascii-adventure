392492aeb057d939e9cdebc5b1439d160d756a44
chloe.spilker <chloe.spilker@CLC-LAB209-13.TCSD.tooelesd.org>
2018-11-19 14:07:56 -0700

    ahh push it

---
+++ src/c/spik3_ascii/Ascii_adventure.java
@@ -0,0 +1,119 @@
+package c.spik3_ascii;
+
+public class Ascii_adventure {
+
+	public static void main(String[] args) {
+		// planning
+		
+		// story
+		/**
+		 * Escape!
+		 * 
+		 * you wake up in a cold damp room
+		 * there is a dangling lamp hanging from the
+		 * ceiling looking like its about the break free from it's
+		 * hinges in the center of the room its bulb looks brand new
+		 * there is a small box on the floor also in the center
+		 * there are no doors
+		 * there are no windows
+		 * to the south there is an abnormal tile
+		 * 
+		 * (player opens box)
+		 * inside the box you find a hammer
+		 * + hammer, box
+		 * 
+		 * (player takes light bulb)
+		 * it burns player's hands a bit
+		 * the entire room goes dim
+		 * your eyes adjust enough to the darkness
+		 * there is a light eminating from an unknown source
+		 * + bulb
+		 * 
+		 * (player goes south)
+		 * the surrounding tiles are all a dull white color, this one,
+		 * however, is the same color as the lamp
+		 *
+		 *		(player breaks tile with hammer)
+		 *		the tile cracks immediately and shards of it fall everywhere
+		 *		one piece is larger than the others and very sharp
+		 *		behind the tile is a safe with a 4 letter code
+		 *
+		 *		(player takes tile shard)
+		 *		the tile is dull enough on the sides to hold, but has
+		 *		a sharp end
+		 *
+		 *		(player breaks safe with hammer or shard)
+		 *		you slam your hammer into the safe, but it bounces back
+		 *		with the safe seemingly unharmed
+		 *	
+		 * (player kills self with tile shard or hammer or gun)
+		 * i guess that's one way to escape
+		 * Ending 1: the easy way out
+		 * 
+		 * (player breaks bulb with hammer)
+		 * the glass of the bulb explodes and shatters all over the floor
+		 * a small roll of paper falls out of the light
+		 * 
+		 * 		(player takes paper)
+		 * 		taken
+		 * 
+		 * 				(player reads paper)
+		 * 				the paper has 8 lines of code in tiny writing
+		 * 				you struggle to read it in the darkness but
+		 *	 			after a few minutes you figure out what it says
+		 * 
+		 * 				char a = "a";
+		 *  			char b = "b";
+		 *  			int z12 = 12;
+		 *  			
+		 *  			if (z12 + 33 == 46) {
+		 *  				System.out.print("two b's");
+		 *  			} else {
+		 *  				System.out.print("two a's"); 
+		 *  
+		 *  
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
+		
+		// actions and nouns
+		/**
+		 * TO INVENTORY
+		 * take
+		 * pick up
+		 * grab
+		 * 
+		 * OPEN CONTAINER
+		 * open
+		 * 
+		 * MOVE
+		 * go
+		 * walk
+		 * 
+		 * look
+		 * 
+		 * ATTACK
+		 * break
+		 * attack
+		 * hit
+		 * kill
+		 * 
+		 * if verb unknown: error unknown verb
+		 * 
+		 * NOUNS
+		 * box
+		 * lamp
+		 * bulb
+		 * hammer
+		 * tile shard
+		 * gun
+		 * roll of paper/paper
+		 * 
+		 * with (two things)
+		 */
+
+	}
+
+}
